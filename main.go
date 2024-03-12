1bc7388f8c77f4c0e586f85ddca34f6ce976ec31
jylkkakai <kiitsu74@gmail.com>
2024-03-12 14:15:26 +0200

    Dense layer and simple backpropagation.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -16,28 +17,42 @@ func main() {
 	const w = 30
 	const c = 32
 
-	inStrArr := readFile("test/inpy.txt")
-	wgtStrArr := readFile("test/wgtpy.txt")
-	outStrArr := readFile("test/outpy.txt")
+	// inStrArr := readFile("test/inpy.txt")
+	// wgtStrArr := readFile("test/wgtpy.txt")
+	// outStrArr := readFile("test/outpy.txt")
 
-	// inArr := strArrToTensor(inStrArr, 1, h, w, c)
-	// wgtArr := strArrToTensor(wgtStrArr, 3, 3, 32, 32)
-	// outArr := strArrToTensor(outStrArr, 1, h, w, c)
-	// fmt.Println(inArr[0][0][0])
-	// fmt.Println(wgtArr[0][0][0])
-	// fmt.Println(outArr[0][0][0])
-	inArr := strArrToFlat(inStrArr, 1, 16)
-	wgtArr := strArrToFlat(wgtStrArr, 32, 16)
-	outArr := strArrToFlat(outStrArr, 1, 32)
-	fmt.Println(inArr)
-	// fmt.Println(wgtArr)
-	fmt.Println(outArr)
-	out := dense(inArr[0], wgtArr, true)
-	fmt.Println(out)
+	// bias := make([]float32, 32)
+	// inArr := strArrToFlat(inStrArr, 1, 16)
+	// wgtArr := strArrToFlat(wgtStrArr, 32, 16)
+	// outArr := strArrToFlat(outStrArr, 1, 32)
+	// fmt.Println(inArr)
+	// fmt.Println(outArr)
+	// out := dense(inArr[0], wgtArr, 0, "")
+	// fmt.Println(out)
+	// out = dense(inArr[0], wgtArr, 1, "relu")
+	// fmt.Println(out)
+	// input := []float32{0.05, 0.1}
+	// w1 := [][]float32{{0.15, 0.2}, {0.25, 0.3}}
+	// w2 := [][]float32{{0.40, 0.45}, {0.50, 0.55}}
+	// bias := []float32{0.35, 0.6}
+	// out1 := dense(input, w1, bias[0], "sigmoid")
+	// fmt.Println(out1)
+	// out2 := dense(out1, w2, bias[1], "sigmoid")
+	denseBackpropagation()
+
 }
 
-func dense(in []float32, wgt [][]float32, relu bool) []float32 {
+func dense(in []float32, wgt [][]float32, bias float32, act string) []float32 {
 
+	var activ activation
+	switch act {
+	case "relu":
+		activ = relu
+	case "sigmoid":
+		activ = sigmoid
+	default:
+		activ = pass
+	}
 	out := make([]float32, len(wgt))
 	for i, k := range wgt {
 		var temp float32 = 0
@@ -47,13 +62,55 @@ func dense(in []float32, wgt [][]float32, relu bool) []float32 {
 			// 	fmt.Println(in[j], k[j], temp)
 			// }
 		}
-		if relu {
-			out[i] = max(0, temp)
-		} else {
-			out[i] = temp
-		}
+		out[i] = activate(temp, bias, activ)
+		// if relu {
+		// 	out[i] = max(0, temp+bias)
+		// } else {
+		// 	out[i] = temp + bias
+		// }
 	}
 	return out
+}
+
+func denseBackpropagation() {
+
+	input := []float32{0.05, 0.1}
+	w1 := [][]float32{{0.15, 0.2}, {0.25, 0.3}}
+	w2 := [][]float32{{0.40, 0.45}, {0.50, 0.55}}
+	bias := []float32{0.35, 0.6}
+
+	net1 := dense(input, w1, bias[0], "")
+	fmt.Println(net1)
+	out1 := dense(input, w1, bias[0], "sigmoid")
+	fmt.Println(out1)
+	net2 := dense(out1, w2, bias[1], "")
+	fmt.Println(net2)
+	out2 := dense(out1, w2, bias[1], "sigmoid")
+	fmt.Println(out2)
+
+	target := []float32{0.01, 0.99}
+	outLoss := loss(out2, target)
+	fmt.Println(outLoss)
+	totalLoss := sumArr(outLoss)
+	fmt.Println("Total loss:", totalLoss)
+
+}
+
+func loss(arr []float32, target []float32) []float32 {
+	resArr := make([]float32, len(arr))
+	for i := range arr {
+		temp := target[i] - arr[i]
+		resArr[i] = 0.5 * temp * temp
+	}
+	return resArr
+}
+
+func sumArr(arr []float32) float32 {
+	result := float32(0.0)
+	for i := range arr {
+		result += arr[i]
+	}
+	return result
 }
 
 // func conv2d(in, wgt tensor, relu, padding bool) tensor {
@@ -68,6 +125,26 @@ func max(x, y float32) float32 {
 	return y
 }
 
+type activation func(float32) float32
+
+func sigmoid(x float32) float32 {
+	return float32(1 / (1 + math.Exp(float64(x)*(-1))))
+}
+func relu(x float32) float32 {
+	return max(0, x)
+}
+func pass(x float32) float32 {
+	return x
+}
+
+func sigmoidDer(x float32) float32 {
+	return x * (1 - x)
+}
+
+func activate(x, b float32, fn activation) float32 {
+	raw := x + b
+	return fn(raw)
+}
 func readFile(fileName string) []string {
 
 	inRead, err := os.ReadFile(fileName)
