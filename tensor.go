7a7f407d8c2543858cf99c6df53572b680da6908
jylkkakai <kiitsu74@gmail.com>
2024-03-23 15:48:17 +0200

    Tensor set method.

--- tensor.go
+++ tensor.go
@@ -24,27 +24,41 @@ func (t *Tensor) at(coord ...int) float32 {
 		ii += temp
 
 	}
-	// fmt.Println(ii)
-	// fmt.Println(t.arr[ii])
 	return t.arr[ii]
 }
+func (t *Tensor) set(x float32, coord ...int) {
+	ii := 0
+	tempm := 1
+	for i := len(t.shape) - 1; i >= 0; i-- {
+		temp := coord[i]
 
+		if i < len(t.shape)-1 {
+			tempm *= t.shape[i+1]
+			temp = tempm * coord[i]
+		}
+		ii += temp
+
+	}
+	t.arr[ii] = x
+}
 func (t *Tensor) readNpy(f string) {
 
 	r, err := gonpy.NewFileReader(f)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	// fmt.Println(err)
-	// fmt.Printf("%T\n", r)
-	// fmt.Println(r.Shape)
-	// fmt.Println(r.Dtype)
 	data, err1 := r.GetFloat32()
 	if err1 != nil {
 		log.Println(err)
 	}
 	t.shape = r.Shape
 	t.arr = data
-	// fmt.Println(err1)
-	// fmt.Println(data)
+}
+func (t *Tensor) zero(s ...int) {
+	len := 1
+	for _, v := range s {
+		len *= v
+	}
+	t.arr = make([]float32, len)
+	t.shape = s
 }
