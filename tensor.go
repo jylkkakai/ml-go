3ec03b7683d3948b83b93b30d9217b8c118e71b7
jylkkakai <kiitsu74@gmail.com>
2024-03-24 15:09:07 +0200

    Add method to tensor.

--- tensor.go
+++ tensor.go
@@ -11,7 +11,7 @@ type Tensor struct {
 	arr   []float32
 }
 
-func (t *Tensor) at(coord ...int) float32 {
+func (t *Tensor) getIndex(coord []int) int {
 	ii := 0
 	tempm := 1
 	for i := len(t.shape) - 1; i >= 0; i-- {
@@ -24,23 +24,19 @@ func (t *Tensor) at(coord ...int) float32 {
 		ii += temp
 
 	}
-	return t.arr[ii]
+	return ii
+}
+func (t *Tensor) at(coord ...int) float32 {
+	return t.arr[t.getIndex(coord)]
 }
 func (t *Tensor) set(x float32, coord ...int) {
-	ii := 0
-	tempm := 1
-	for i := len(t.shape) - 1; i >= 0; i-- {
-		temp := coord[i]
-
-		if i < len(t.shape)-1 {
-			tempm *= t.shape[i+1]
-			temp = tempm * coord[i]
-		}
-		ii += temp
-
-	}
-	t.arr[ii] = x
+	t.arr[t.getIndex(coord)] = x
 }
+
+func (t *Tensor) add(x float32, coord ...int) {
+	t.arr[t.getIndex(coord)] += x
+}
+
 func (t *Tensor) readNpy(f string) {
 
 	r, err := gonpy.NewFileReader(f)
